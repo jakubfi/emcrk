2582870ffb696b041c5f54ee695169f1376b0291
Jakub Filipowicz <jakubf@gmail.com>
2022-02-13 13:56:15 +0100

    update windows build

--- .ci/ci-script.sh
+++ .ci/ci-script.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
-cmake -E make_directory "$BUILD_DIR"
-cmake -B "$SRC_DIR" -DCMAKE_BUILD_TYPE="$BUILD_TYPE" -G "Unix Makefiles"
-cmake --build "$BUILD_DIR"
-sudo cmake --install "$BUILD_DIR"
+
+ACTION=$1
+BUID_DIR=$SRC_DIR/build
+
+case $ACTION in
+
+	init)
+		cmake -E make_directory "$BUILD_DIR"
+	;;
+
+	configure)
+		cmake -B "$BUILD_DIR" -DCMAKE_BUILD_TYPE="$BUILD_TYPE" -G "Unix Makefiles" "$SRC_DIR"
+	;;
+
+	build)
+		cmake --build "$BUILD_DIR"
+	;;
+
+	install)
+		sudo cmake --install "$BUILD_DIR"
+	;;
+
+	*)
+		echo "Unknown action: $ACTION"
+		exit 1
+	;;
+
+esac
+
+# vim: tabstop=4
